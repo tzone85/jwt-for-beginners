3c21e683ef531b06884f0adf56a42a5ad8609cb7
thando mini <tzone85@gmail.com>
2019-09-12 00:48:08 +0200

    started up an express server and logged to the console and the browser msgs confirming that it is up

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var express = require("express");
 
 var app = express()
 
+app.get("/", function (req, res) {
+    res.json({message: "Express is up!"});
+});
+
+app.listen(3000, function () {
+    console.log("Express running");
+});
