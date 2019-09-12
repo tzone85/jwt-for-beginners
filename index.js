59636192977effd8aa4d0af51d42d19b7801571b
thando mini <tzone85@gmail.com>
2019-09-12 17:51:47 +0200

    added the passport jwt strategy and logged the payload just for debugging purposes

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+var _ = require('lodash');
 var express = require("express");
+var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken');
 
-var app = express()
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var app = express();
+
+var users = [
+    {
+        id: 1,
+        name: 'mncedi',
+        password: 'testing'
+    },
+    {
+        id: 2,
+        name: 'thando',
+        password: 'testing'
+    }
+];
+
+
+// passport jwt strategy
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.sec_secretOrKeyProvider = 'thandomncedimini';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    // normally the following would be a database call
+    var user = users[_.findIndex((users, {id: jwt_payload.id}))];
+
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
 
 app.get("/", function (req, res) {
     res.json({message: "Express is up!"});
