fe4b198c2594196683cd53f06464467d1dd88eb8
thando mini <tzone85@gmail.com>
2019-09-13 12:48:28 +0200

    ammended login route g2g

--- index.js
+++ index.js
@@ -59,6 +59,28 @@ app.get("/", function (req, res) {
     res.json({message: "Express is up!"});
 });
 
+app.post("/login", function(req, res) {
+    if (req.body.name && req.body.password) {
+        var name = req.body.name;
+        var password = req.body.password;
+    }
+
+    // usually this is a DB call
+    var user = users[_.findIndex(users, {name: name})];
+    if (! user ){
+        res.status(401).json({message: "No such user found"});
+    }
+
+    if (user.password === req.body.password) {
+        // from now on, we'll identify the user by the id and the id is the only personalized value going into our token
+        var payload = {id: user.id};
+        var token = jwt.sign(payload, jwtOptions.sec_secretOrKeyProvider);
+        res.json({message: "ok", token: token});
+    } else {
+        res.status(401).json({message: "passwords did not match"});
+    }
+});
+
 app.listen(3000, function () {
     console.log("Express running");
 });
